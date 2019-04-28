4c74261df35f6ecfc058f247a2c2e800d0db5c08
Mbertschi <michibertschi@gmail.com>
2019-04-28 20:53:29 +0200

    UI version one

--- src/main/java/UI/MainApp.java
+++ src/main/java/UI/MainApp.java
@@ -12,7 +12,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("start.fxml"));
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
