36b350becd28824fc6190d56c4b977c8990dea3b
Rehan Beg Mirza <rehanbeg.mirza@juventus.schule>
2019-04-30 21:07:09 +0200

    Test with Click Event AddPlayerAmount

--- src/main/java/UI/FXMLController.java
+++ src/main/java/UI/FXMLController.java
@@ -1,21 +1,43 @@
 package UI;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import business.MainController;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import javax.swing.*;
 
 public class FXMLController implements Initializable {
 
     @FXML
     private Label label;
 
+    @FXML
+    private int amountPlayerTextfield;
+
+    @FXML
+    private Button addAmountButton;
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+        //String javaVersion = System.getProperty("java.version");
+        //String javafxVersion = System.getProperty("javafx.version");
+        //label.setText("Hello, JavaFX " + javafxVersion + "\nRunning on Java " + javaVersion + ".");
+
+        //addAmountButton.(continueAmountPlayerButton();
     }
+
+
+    public void continueAmountPlayerButton() {
+        MainController mainController = new MainController();
+        mainController.SetAmountPlayer(amountPlayerTextfield);
+    }
+
 }
