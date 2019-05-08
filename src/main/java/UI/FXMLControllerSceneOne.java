0d8931f961f60b866d750b0af43b7524a18b7c0f
Mbertschi <michibertschi@gmail.com>
2019-05-08 22:13:31 +0200

    add methods to all FXMLController

--- src/main/java/UI/FXMLControllerSceneOne.java
+++ src/main/java/UI/FXMLControllerSceneOne.java
@@ -30,20 +30,6 @@ public class FXMLControllerSceneOne {
     @FXML
     private TextField amountPlayerTextfield;
 
-    @FXML
-    private Button addAmountButton;
-
-    @FXML
-    private Button backToSceneTwo;
-
-//    //@Override
-//    public void initialize(URL url, ResourceBundle rb) {
-//        addAmountButton.setOnAction((event) -> {
-//            label1.setText("Button click hat funktioniert :)");
-//            continueAmountPlayerButton();
-//
-//        });
-//    }
 
     // Go to SceneTwo
     public void addAmountButton(ActionEvent event) throws IOException {
@@ -58,77 +44,6 @@ public class FXMLControllerSceneOne {
         window.show();
 
     }
-    //Go to SceneTree
-    public void toSceneTree(ActionEvent event) throws IOException {
-        Parent sceneTree = FXMLLoader.load(getClass().getResource("SceneTreePlayerList.fxml"));
-
-        Scene windowSceneTwo = new Scene(sceneTree);
-        // Stage Information
-
-        Stage window2 = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window2.setScene(windowSceneTwo);
-        window2.show();
-
-    }
-
-    //Back to SceneOne
-    public void backToSceneOne(ActionEvent event) throws IOException {
-        Parent sceneTwo = FXMLLoader.load(getClass().getResource("start.fxml"));
-
-        Scene windowSceneTwo = new Scene(sceneTwo);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(windowSceneTwo);
-        window.show();
-
-    }
-
-    //Go to SceneFive
-    public void toSceneFive(ActionEvent event) throws IOException {
-        Parent sceneFive = FXMLLoader.load(getClass().getResource("SceneFivePlayField.fxml"));
-
-        Scene windowSceneFive = new Scene(sceneFive);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(windowSceneFive);
-        window.show();
-
-    }
-
-    // Back to SceneTwo
-    public void backToSceneTwo(ActionEvent event) throws IOException {
-        Parent sceneTwo = FXMLLoader.load(getClass().getResource("SceneTwoPlayerName.fxml"));
-
-        Scene windowSceneTwo = new Scene(sceneTwo);
-        // Stage Information
-
-        Stage window2 = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window2.setScene(windowSceneTwo);
-        window2.show();
-
-    }
-
-    // Back to SceneSix
-    public void toSceneSix(ActionEvent event) throws IOException {
-        Parent sceneSix = FXMLLoader.load(getClass().getResource("SceneSixRankingAndRestart.fxml"));
-
-        Scene windowSceneSix = new Scene(sceneSix);
-        // Stage Information
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(windowSceneSix);
-        window.show();
-
-    }
-
-
-
-
 
     private void continueAmountPlayerButton() {
         MainController mainController = new MainController();
--- src/main/java/UI/FXMLControllerSceneTree.java
+++ src/main/java/UI/FXMLControllerSceneTree.java
@@ -1,4 +1,32 @@
 package UI;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class FXMLControllerSceneTree {
+    // Back to SceneTwo
+    public void backToSceneTwo(ActionEvent event) throws IOException {
+        Parent sceneTwo = FXMLLoader.load(getClass().getResource("SceneTwoPlayerName.fxml"));
+        Scene windowSceneTwo = new Scene(sceneTwo);
+        // Stage Information
+        Stage window2 = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window2.setScene(windowSceneTwo);
+        window2.show();
+
+    }
+    //Go to SceneFive
+    public void goToSceneFive(ActionEvent event) throws IOException {
+        Parent sceneFive = FXMLLoader.load(getClass().getResource("SceneFivePlayField.fxml"));
+        Scene windowSceneFive = new Scene(sceneFive);
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(windowSceneFive);
+        window.show();
+
+    }
 }
